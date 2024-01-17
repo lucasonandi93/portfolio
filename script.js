d2687577423369f1e5fd842ae03227620c8a18bc
lucasonandi93 <lucas.onandi2022@campus-eni.fr>
2024-01-17 05:48:17 +0100

    contacto

--- script.js
+++ script.js
@@ -30,19 +30,31 @@ const toggleText = document.getElementById('toggle-text');
 
 const flgsElement = document.getElementById("flags");
 
-const textsToChange = document.querySelectorAll("[data-section]")
+const elementsToChange = document.querySelectorAll("[data-section], [data-placeholder]");
 
-const changeLanguage = async language=>{
+const changeLanguage = async (language) => {
     const requestJson = await fetch(`./languages/${language}.json`);
     const texts = await requestJson.json();
-
-    for(const textToChange of textsToChange){
-        const section = textToChange.dataset.section;
-        const value = textToChange.dataset.value;
-
-        textToChange.innerHTML = texts[section][value];
+  
+    for (const elementToChange of elementsToChange) {
+      // Obtiene los atributos data-section, data-placeholder y data-value
+      const section = elementToChange.dataset.section;
+      const placeholder = elementToChange.dataset.placeholder;
+      const value = elementToChange.dataset.value;
+  
+      // Verifica si el elemento tiene el atributo data-section
+      if (section) {
+        // Actualiza el innerHTML del elemento con el texto correspondiente de los datos JSON
+        elementToChange.innerHTML = texts[section][value];
+      }
+  
+      // Verifica si el elemento tiene el atributo data-placeholder
+      if (placeholder) {
+        // Actualiza el atributo placeholder del elemento con el texto correspondiente de los datos JSON
+        elementToChange.setAttribute('placeholder', texts[placeholder][value]);
+      }
     }
-};
+  };
 
 flgsElement.addEventListener("click", (e) => {
     changeLanguage(e.target.parentElement.dataset.language);
